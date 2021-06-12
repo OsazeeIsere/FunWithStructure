69f0b59620af5f5a3b5ffba2544886bf79177234
Osazee Isere <oz1.concepts@gmail.com>
2021-06-12 17:43:58 +0100

    fun with structure

--- FunWithStructure/Program.cs
+++ FunWithStructure/Program.cs
@@ -8,8 +8,60 @@ namespace FunWithStructure
 {
     class Program
     {
+        struct Point
+        {
+
+            // fields of the structure
+            public int x;
+            public int y;
+
+            // add 1 to the (x,y) position
+            public void Increment()
+            {
+                x++; y++;
+            }
+
+            //subtract 1 from x and y position
+            public void Decrement()
+            {
+                x--; y--;
+            }
+
+            //display the current position
+            public void Display()
+            {
+                Console.WriteLine("X={0}, Y={1}", x, y);
+                Console.WriteLine();
+            }
+
+            //a custom constructor
+            public void CustomConstructor(int xPos, int yPos)
+            {
+                x = xPos;
+                y = yPos;
+            }
+        }
         static void Main(string[] args)
         {
+
+            Console.WriteLine("=> a first look at structure");
+
+            //creat an initial point
+            Point myPoint;
+            myPoint.x = 455;
+            myPoint.y = 43;
+            myPoint.Display();
+
+            //adjust the x and y values
+            myPoint.Increment();
+            myPoint.Display();
+
+            //calling custom Constructor
+            myPoint.CustomConstructor(50, 60);
+            myPoint.Display();
+
+            Console.ReadLine();
+
         }
     }
 }
